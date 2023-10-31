d749dfbf2e013fa997550f3e0ba0804494072b38
Francisco <OcampoCr722@gmail.com>
2023-10-31 13:36:16 -0500

    Initial commit

---
+++ Breakk statment.py
@@ -0,0 +1,8 @@
+count = 1
+while count <= 5
+    print(count)
+    if count == 3:
+        count += 1
+        continue
+        print(count)
+        count += 1
---
+++ main.py
@@ -0,0 +1,7 @@
+x = 0
+while x < 1000000:
+    print(x)
+    if x == 1000:
+        break
+        x += 1
+
